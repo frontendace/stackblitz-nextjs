6ec5ad9e733723d7a00c3a9c4b29707b8dcc6d79
frontendace <shivam12112roy@gmail.com>
2025-01-30 16:02:52 +0530

    start writing code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Read description of Challenge and write code"</h1>
+      <h1 className='text-2xl'>Read description of Challenge and write code</h1>
     </main>
   );
 }
